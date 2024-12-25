b990ad1c18e74749e64d5c681b531c2257db2c7c
JHS <skskfl5786@gmail.com>
2024-12-25 20:37:21 +0900

    Title: 출석 부르기, Time: 135ms, Memory: 10MB, Status: Passed - CodeTree

---
+++ 241225/출석 부르기/calling-attendance.java
@@ -0,0 +1,14 @@
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+        // 여기에 코드를 작성해주세요.
+        Scanner sc = new Scanner(System.in);
+        int n = sc.nextInt();
+
+        if (n == 1) System.out.print("John");
+        else if (n == 2) System.out.print("Tom");
+        else if (n == 3) System.out.print("Paul");
+        else System.out.print("Vacancy");
+    }
+}
