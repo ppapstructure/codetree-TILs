864d0121a3f230cfdca1ada21d67db6ef08613fb
JHS <skskfl5786@gmail.com>
2024-12-29 17:16:00 +0900

    Title: 20대의 평균 나이, Time: 127ms, Memory: 10MB, Status: Runtime Error - CodeTree

---
+++ 241229/20대의 평균 나이/average-age-of-20.java
@@ -0,0 +1,22 @@
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+        // 여기에 코드를 작성해주세요.
+        Scanner sc = new Scanner(System.in);
+        int n;
+        int sumVal=0;
+        int cnt = 0;
+        double avg;
+   
+
+    while(true) {
+        n = sc.nextInt();
+        if(n>=30) break;
+        sumVal += n;
+        cnt++;
+    }
+    avg = (double)sumVal/cnt;
+    System.out.printf("%.2f",avg);
+    }
+}
