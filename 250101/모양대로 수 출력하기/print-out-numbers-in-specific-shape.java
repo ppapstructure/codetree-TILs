d6b36ac728a9e1611f9fa108eda982af8c5772de
JHS <skskfl5786@gmail.com>
2025-01-01 23:40:55 +0900

    Title: 모양대로 수 출력하기, Time: 128ms, Memory: 10MB, Status: Passed - CodeTree

---
+++ 250101/모양대로 수 출력하기/print-out-numbers-in-specific-shape.java
@@ -0,0 +1,18 @@
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+        // 여기에 코드를 작성해주세요.
+        Scanner sc = new Scanner(System.in);
+        int n = sc.nextInt();
+
+        for(int i = 0;i<n;i++) {
+            for(int j = 0;j<n;j++) {
+                if(j>=i) System.out.print(n-j+" ");
+                else System.out.print("  ");
+            }
+            System.out.println();
+        }
+
+    }
+}
