d128affe1a6c3386e06b421b872b4ee989674a49
JHS <skskfl5786@gmail.com>
2025-01-02 16:20:28 +0900

    Title: 평균점수 구하기, Time: 134ms, Memory: 12MB, Status: Passed - CodeTree

---
+++ 250102/평균점수 구하기/print-average.java
@@ -0,0 +1,19 @@
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+        // Please write your code here.
+        Scanner sc = new Scanner(System.in);
+        double[] arr = new double[8];
+        double avg = 0;
+        double sum = 0;
+        
+        for(int i = 0;i<8;i++) {
+            arr[i] = sc.nextDouble();
+            sum += arr[i];
+        }
+        avg = sum/8;
+
+        System.out.printf("%.1f", avg);
+    }
+}
