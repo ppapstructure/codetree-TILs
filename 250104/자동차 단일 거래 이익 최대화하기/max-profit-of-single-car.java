a63957e8f84eef297da3a79cdb82267864c65bd6
JHS <skskfl5786@gmail.com>
2025-01-04 21:16:42 +0900

    Title: 자동차 단일 거래 이익 최대화하기, Time: 203ms, Memory: 12MB, Status: Passed - CodeTree

--- 250104/자동차 단일 거래 이익 최대화하기/max-profit-of-single-car.java
+++ 250104/자동차 단일 거래 이익 최대화하기/max-profit-of-single-car.java
@@ -12,43 +12,15 @@ public class Main {
             arr[i] = sc.nextInt();
         }
         int maxVal = 0;
-        int minVal = arr[0];
-        int idx_min=-1;
 
-        // 최저 가격 찾기
         for(int i = 0;i<n;i++) {
-            if(arr[i]<minVal) {
-                minVal = arr[i];
-                idx_min = i;
-            }
-        }
-        // System.out.println(minVal);
-
-        // 최솟값이 배열에 끝에 나온다면 그 다음으로 작은 최솟값을 구한다.
-        if (idx_min == n-1) {
-            int ne = idx_min;
-            minVal = arr[0];
-            for(int i = 0;i<ne;i++) {
-                if(arr[i]<minVal) {
-                    minVal = arr[i];
-                    idx_min = i;
-                }
+            for(int j = i+1;j<n;j++) {
+                if(maxVal < arr[j]-arr[i]) maxVal = arr[j]-arr[i];
             }
         }
 
-        // 최대 이익 구하기
-        for(int i = idx_min+1;i<n;i++) {
-            if(arr[i]>maxVal) {
-                maxVal = arr[i];
-            }
-        }
+        if(maxVal<=0) System.out.print(0);
+        else System.out.print(maxVal);
 
-
-        if(maxVal-minVal <= 0) {
-            System.out.print(0);
-        }
-        else {
-        System.out.print(maxVal-minVal);
-        }
     }
 }
