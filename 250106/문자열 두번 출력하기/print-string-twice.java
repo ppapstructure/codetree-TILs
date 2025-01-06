0861d1c325a2536bffab36204073a6f69c287c7a
JHS <skskfl5786@gmail.com>
2025-01-06 21:06:45 +0900

    Title: 문자열 두번 출력하기, Time: 122ms, Memory: 10MB, Status: Passed - CodeTree

---
+++ 250106/문자열 두번 출력하기/print-string-twice.java
@@ -0,0 +1,15 @@
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+        // Please write your code here.
+        Scanner sc = new Scanner(System.in);
+        
+        String arr;
+        arr = sc.next();
+
+        System.out.println(arr);
+        System.out.println(arr);
+
+    }
+}
