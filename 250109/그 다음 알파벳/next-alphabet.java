e3dfeecbc3bcca4455d0ffebe607a607deb3d48c
JHS <skskfl5786@gmail.com>
2025-01-09 13:50:08 +0900

    Title: 그 다음 알파벳, Time: 118ms, Memory: 10MB, Status: Passed - CodeTree

---
+++ 250109/그 다음 알파벳/next-alphabet.java
@@ -0,0 +1,18 @@
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+        // Please write your code here.
+        Scanner sc = new Scanner(System.in);
+        char c = sc.next().charAt(0);
+
+        if(c=='z') {
+            System.out.print('a');
+        }
+        else {
+            System.out.print((char)(c+1));
+        }
+
+
+    }
+}
