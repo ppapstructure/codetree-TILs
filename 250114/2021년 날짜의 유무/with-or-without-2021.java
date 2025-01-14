7b8e439e5178af4260286102427a6d1af4287d5d
JHS <skskfl5786@gmail.com>
2025-01-14 16:21:57 +0900

    Title: 2021년 날짜의 유무, Time: 120ms, Memory: 10MB, Status: Passed - CodeTree

--- 250114/2021년 날짜의 유무/with-or-without-2021.java
+++ 250114/2021년 날짜의 유무/with-or-without-2021.java
@@ -4,16 +4,19 @@ public class Main {
 
     public static boolean Day(int m, int n) {
         if(m==2) {
-            if(n>=1&&n<=28) return true;
+            if(n>=1 && n<=28) return true;
+            else return false;
         }
 
         if(m<8) {
             if(m%2!=0) {
                 if(n>=1 && n<=31) return true;
+                else return false;
             }
 
             else {
                 if(n>=1 && n<=30) return true;
+                else return false;
             }
 
         }
@@ -21,9 +24,11 @@ public class Main {
         else if (m>=8&&m<=12) {
             if(m%2==0) {
                 if(n>=1 && n<=31) return true;
+                else return false;
             }
             else {
                 if(n>=1 && n<=30) return true;
+                return false;
             }
         }
 
