41f61d13fd6b144c68e3a25e102c83d9ba04d7c6
JHS <skskfl5786@gmail.com>
2025-01-16 13:58:31 +0900

    Title: Next Level, Time: 141ms, Memory: 10MB, Status: Passed - CodeTree

---
+++ 250116/Next Level/next-level.java
@@ -0,0 +1,31 @@
+import java.util.Scanner;
+
+class Account {
+    String id;
+    int level;
+
+    public Account() {
+        this.id = "codetree";
+        this.level = 10;
+    }
+    
+    public Account(String id, int level) {
+        this.id = id;
+        this.level = level;
+    }
+
+}
+
+public class Main {
+    public static void main(String[] args) {
+        // Please write your code here.
+        Scanner sc = new Scanner(System.in);
+        String s = sc.next();
+        int lev = sc.nextInt();
+
+        Account user1 = new Account();
+        System.out.printf("user %s lv %d\n", user1.id, user1.level);
+        Account user2 = new Account(s, lev);
+        System.out.printf("user %s lv %d\n", user2.id, user2.level);
+    }
+}
