3ed0c0ec4de71fda689069966e0984e825a672f0
JHS <skskfl5786@gmail.com>
2025-01-21 19:56:48 +0900

    Title: 문자열 정렬, Time: 115ms, Memory: 10MB, Status: Passed - CodeTree

---
+++ 250121/문자열 정렬/string-sort.java
@@ -0,0 +1,14 @@
+import java.util.Scanner;
+import java.util.Arrays;
+
+public class Main {
+    public static void main(String[] args) {
+        // Please write your code here.
+        Scanner sc = new Scanner(System.in);
+        String str = sc.next();
+        char[] chars = str.toCharArray();
+        Arrays.sort(chars);
+        String sortedStr = new String(chars);
+        System.out.println(sortedStr);
+    }
+}
