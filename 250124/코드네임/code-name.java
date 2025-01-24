4bf872ffa353cc1191f3d2bdd3e05c7abd057731
JHS <skskfl5786@gmail.com>
2025-01-24 12:36:45 +0900

    Title: 코드네임, Time: 124ms, Memory: 10MB, Status: Passed - CodeTree

---
+++ 250124/코드네임/code-name.java
@@ -0,0 +1,79 @@
+// import java.util.Scanner;
+
+// class Agent {
+//     char codename;
+//     int score;
+
+//     public Agent(char codename, int score) {
+//         this.codename = codename;
+//         this.score = score;
+//     }
+// }
+
+// public class Main {
+//     public static void main(String[] args) {
+//         Scanner sc = new Scanner(System.in);
+//         Agent[] agent = new Agent[5];
+
+//         for(int i = 0;i<5;i++) {
+//             char codename = sc.next().charAt(0);
+//             int score = sc.nextInt();
+//             agent[i] = new Agent(codename, score);
+//         }
+
+//         int min = agent[0].score;
+//         char min_codename = agent[0].codename;
+
+//         for(int i = 0;i<5;i++) {
+//             if(min>agent[i].score) {
+//                 min = agent[i].score;
+//                 min_codename = agent[i].codename;
+//             }
+//         }
+
+//         System.out.print(min_codename + " " + min);
+//     }
+// }
+
+import java.util.Scanner;
+
+class Agent {
+    char codename;
+    int score;
+
+    public Agent() {
+        this.codename = ' ';
+        this.score = 0;
+    }
+
+    public Agent(char codename, int score) {
+        this.codename = codename;
+        this.score = score;
+    }
+
+}
+
+public class Main {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        Agent[] agent = new Agent[5];
+        
+        for(int i = 0;i<5;i++) {
+            char codename = sc.next().charAt(0);
+            int score = sc.nextInt();
+            agent[i] = new Agent(codename, score);
+        }
+
+        int minVal = agent[0].score;
+        char minCode = agent[0].codename;
+
+        for(int i = 0;i<5;i++) {
+            if(minVal > agent[i].score) {
+                minCode = agent[i].codename;
+                minVal = agent[i].score;
+            }
+        }
+
+        System.out.print(minCode+" " +minVal);
+    }
+}
