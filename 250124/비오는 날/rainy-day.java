c649fa25406a5daadc4840f0a82f9e88ace1e50b
JHS <skskfl5786@gmail.com>
2025-01-24 17:02:55 +0900

    Title: 비오는 날, Time: 121ms, Memory: 10MB, Status: Passed - CodeTree

--- 250124/비오는 날/rainy-day.java
+++ 250124/비오는 날/rainy-day.java
@@ -18,17 +18,28 @@ public class Main {
     public static final int MAX_N = 100;
     public static Rain[] data = new Rain[MAX_N];
 
-    public static boolean func(String date1, String date2) {
-        int len = date1.length();
+public static boolean func(String date1, String date2) {
+    // 1. 길이가 다르면, 둘 중 더 짧은 쪽이 사전순으로 앞설 가능성이 있으므로
+    //    두 문자열 중 더 작은 길이를 기준으로 반복
+    int len = Math.min(date1.length(), date2.length());
 
-        for(int i = 0;i<len;i++) {
-            if(date1.charAt(i)<date2.charAt(i)){
-                return false;
-            }
+    for (int i = 0; i < len; i++) {
+        char c1 = date1.charAt(i);
+        char c2 = date2.charAt(i);
+
+        if (c1 > c2) {
+            // date1이 date2보다 사전순으로 앞서므로 true
+            return true;
+        } else if (c1 < c2) {
+            // date1이 date2보다 사전순으로 뒤이므로 false
+            return false;
         }
-
-        return true;
     }
+
+    // 여기까지 내려왔다는 것은 앞쪽 글자가 모두 같다는 의미
+    // 따라서 길이가 더 짧은 쪽이 사전순으로 앞선다
+    return date1.length() < date2.length();
+}
 
     public static void main(String[] args) {
         // Please write your code here.
